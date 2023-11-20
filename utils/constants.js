7dab41dfb5174b56d8a4fee52736ef1462218372
Anton Morohin <AMorohin@yandex.ru>
2023-11-20 21:32:30 +0300

    Отправка на ревью

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://amoro-diplom-front.nomoredomainsrocks.ru'],
+  origin: 'https://amoro-diplom-front.nomoredomainsrocks.ru',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   credentials: true,
