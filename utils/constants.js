9987624128edc98e71c24722e897976ba4e1e8ed
Anton Morohin <AMorohin@yandex.ru>
2023-11-21 20:11:06 +0300

    cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,16 +1,20 @@
 const rateLimit = require('express-rate-limit');
 
-const corsOptions = {
-  origin: 'https://amoro-diplom-front.nomoredomainsrocks.ru',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:3000',
+//     'https://amoro-diplom-front.nomoredomainsrocks.ru',
+//   ],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+//   credentials: true,
+// };
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 1000,
 });
 
-module.exports = { corsOptions, limiter };
+module.exports = { limiter };
